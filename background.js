fe43fe2512062cb7545f5da36603b1a6a2c03386
Jacob Strieb <jstrieb@users.noreply.github.com>
2020-07-29 02:51:57 -0400

    Add comments

--- background.js
+++ background.js
@@ -9,17 +9,26 @@ function activateButton(tab) {
 }
 
 
+/**
+ * Called when a tab gets "updated." Most importantly, this includes when a
+ * user clicks a link.
+ */
 function updateTab(tabId, changeInfo, tab) {
+  // Only submit URLs of pages that have completed loading
   if (!("status" in changeInfo && changeInfo.status == "complete")) {
     return;
   }
+
+  // Ignore built-in Firefox "about:" pages
   var tab_url = new URL(tab.url);
   if (tab_url.protocol == "about:") {
     return;
   }
 
+  // Fetch a search for the current page and parse the JSON result
   fetch(`https://hn.algolia.com/api/v1/search?tags=story&query=${tab.url}`)
     .then(data => data.json().then(json => {
+      // Filter out stories whose URL doesn't match the current one
       var stories = Array.from(json.hits).filter(hit => {
         if (hit == null) return false;
         try {
@@ -32,17 +41,20 @@ function updateTab(tabId, changeInfo, tab) {
           && tab_url.pathname == hit_url.pathname
           && tab_url.search == hit_url.search);
       });
+
+      // If a story matched, set its value in the dictionary
       if (stories.length > 0) {
         tabs[tabId] = stories[0];
         return;
       }
-      // TODO: Add fuzzy title matching for inexact URLs
+
+      // TODO: Fall back on fuzzy title matching for inexact URLs
     }));
 }
 
 
 
-// Main procedure called on initialization
+// Main procedure; called on initialization
 browser.tabs.onRemoved.addListener(tabId => delete tabs[tabId]);
 browser.tabs.onActivated.addListener(activateButton);
 browser.tabs.onUpdated.addListener(updateTab);
