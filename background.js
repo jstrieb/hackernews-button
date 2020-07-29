01f1e2196d1989cf8a11d9393b4300ba74199b7e
Jacob Strieb <jstrieb@users.noreply.github.com>
2020-07-29 02:48:06 -0400

    Minor improvements

--- background.js
+++ background.js
@@ -9,33 +9,40 @@ function activateButton(tab) {
 }
 
 
-async function updateTab(tabId, changeInfo, tab) {
+function updateTab(tabId, changeInfo, tab) {
   if (!("status" in changeInfo && changeInfo.status == "complete")) {
     return;
   }
+  var tab_url = new URL(tab.url);
+  if (tab_url.protocol == "about:") {
+    return;
+  }
+
   fetch(`https://hn.algolia.com/api/v1/search?tags=story&query=${tab.url}`)
     .then(data => data.json().then(json => {
-      var tab_url = new URL(tab.url);
-      var filtered = Array.from(json.hits).filter(hit => {
-        var hit_url = new URL(hit.url);
+      var stories = Array.from(json.hits).filter(hit => {
+        if (hit == null) return false;
+        try {
+          var hit_url = new URL(hit.url);
+        } catch (err) {
+          console.error("Failed on " + hit.url);
+          return false;
+        }
         return (tab_url.host == hit_url.host
           && tab_url.pathname == hit_url.pathname
           && tab_url.search == hit_url.search);
       });
-      if (filtered.length > 0) {
-        tabs[tabId] = filtered[0];
-        console.log(filtered[0]);
+      if (stories.length > 0) {
+        tabs[tabId] = stories[0];
+        return;
       }
+      // TODO: Add fuzzy title matching for inexact URLs
     }));
 }
 
 
 
 // Main procedure called on initialization
-
-browser.tabs.query({})
-  .then(tabList => Array.from(tabList).map(tab => tabs[tab.id] = null));
 browser.tabs.onRemoved.addListener(tabId => delete tabs[tabId]);
-browser.tabs.onCreated.addListener(tab => tabs[tab.id] = null);
 browser.tabs.onActivated.addListener(activateButton);
 browser.tabs.onUpdated.addListener(updateTab);
